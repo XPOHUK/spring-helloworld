88ca7cb5209c5fac32c8dda59f94b5075bb0ac90
Dmitriy Gubankov <dmitriy@gubankov.ru>
2021-03-07 11:24:07 +0300

    Implemented

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,12 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println("HelloWorld1 equals HelloWorld2 ? " + (bean == bean2));
+        System.out.println("Cat1 equals Cat2 ? " + (cat == cat2));
         System.out.println(bean.getMessage());
     }
 }
